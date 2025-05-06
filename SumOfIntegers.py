d84fb55320f074c9881c3eb9d0714f296fffe9e3
VelandiaJuan <ryu.mitsuky.87@gmail.com>
2025-05-06 09:40:02 -0500

    New Folder

---
+++ SumOfIntegers.py
@@ -0,0 +1,20 @@
+
+
+
+###Suma de dos numeros enteros####
+####Primero definimos las variables#####
+n1=int (input("Ingrese el primer valor: "))
+n2=int (input("Ingrese el segundo valo: "))
+
+
+#####Luego se hace la suma######
+Suma = n1+n2 
+
+####Por ultimo se escribe el resultado######
+print ("La suma de los dos valores es: ", Suma)
+
+
+####Por ultimo se puede añadir un cronometro para que poder visualizar el resultado####
+import time
+time.sleep(2)
+#####Creado por VelandiaJuan#####
